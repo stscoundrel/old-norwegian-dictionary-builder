7478af7ea15918b0621ecf6552671717eafa3f15
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-01-10 15:21:03 +0200

    Add writer module & method to write dictionary to json

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/stscoundrel/old-norwegian-dictionary-builder/parser"
+	"github.com/stscoundrel/old-norwegian-dictionary-builder/reader"
+	"github.com/stscoundrel/old-norwegian-dictionary-builder/writer"
+)
 
 func dummyMethod() string {
 	return "Lorem ipsum dolor sit amet"
 }
 
+func ToJson() {
+	lines := reader.ReadLinesFromDictionary()
+	entries := parser.ParseLines(lines)
+
+	writer.WriteJson("build/dictionary.json", entries)
+}
+
 func main() {
-	fmt.Println(dummyMethod())
+	ToJson()
 }
